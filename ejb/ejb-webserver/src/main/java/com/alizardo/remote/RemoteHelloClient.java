c91ef58c3baddc67ed4aa07e20615b3a44f941fa
Andre Lizardo <alizardo@redhat.com>
2025-01-29 10:40:18 +0100

    add recent work

--- ejb/ejb-webserver/src/main/java/com/alizardo/remote/RemoteHelloClient.java
+++ ejb/ejb-webserver/src/main/java/com/alizardo/remote/RemoteHelloClient.java
@@ -10,10 +10,15 @@ public class RemoteHelloClient implements RemoteHello {
     public InitialContext createContext() throws NamingException {
         Properties props = new Properties();
         props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-        props.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+        props.put(Context.PROVIDER_URL, "remote+http://192.168.1.41:8080");
+        //props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        //props.put("wildfly.naming.client.ejb.context", true);
+        //props.put("org.jboss.ejb.client.scoped.context", true); // enable scoping here
         return new InitialContext(props);
     }
 
+    // org.wildfly.naming.client.store.RelativeContext cannot be cast to class com.alizardo.remote.RemoteHello
+    // java.lang.IllegalArgumentException: COM00008: Parameter 'moduleName' must not be empty
     public RemoteHello lookup(InitialContext context) throws NamingException {
         final String rcal = "ejb:/ejb-server/RemoteHello!com.alizardo.remote.RemoteHello";
         return (RemoteHello) context.lookup(rcal);
@@ -22,9 +27,11 @@ public class RemoteHelloClient implements RemoteHello {
     @Override
     public String sayHello() {
         try {
-            final RemoteHello remote = lookup(createContext());
+            InitialContext context = createContext();
+            System.out.println("Context created");
+            final RemoteHello remote = lookup(context);
+            System.out.println("Lookup successful");
             final String result = remote.sayHello();
-
             System.out.println("sayHello succeed: " + result);
             return result;
         } catch (NamingException e) {
